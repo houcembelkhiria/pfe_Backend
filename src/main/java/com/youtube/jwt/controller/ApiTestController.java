35313789f3d18bb117e754887ed13a741ff3a446
houcembelkhiria <119082763+houcembelkhiria@users.noreply.github.com>
2023-05-14 15:59:41 +0100

    this is

--- src/main/java/com/youtube/jwt/controller/ApiTestController.java
+++ src/main/java/com/youtube/jwt/controller/ApiTestController.java
@@ -4,15 +4,19 @@ package com.youtube.jwt.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.mongodb.client.MongoCollection;
+import com.mongodb.client.*;
+import com.youtube.jwt.entity.ApiTest;
 import org.bson.Document;
+import org.json.JSONObject;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 public class ApiTestController {
@@ -26,31 +30,136 @@ public class ApiTestController {
         this.collection = mongoTemplate.getCollection("apiTestResults");
     }
 
-    @PostMapping("/storeTestResults")
-    public ResponseEntity<String> storeResult(@RequestBody String result) {
+   /* @PostMapping("/storeTestResults")
+    public ResponseEntity<String> storeResult(@RequestBody String result, HttpServletRequest request) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode rootNode = objectMapper.readTree(result);
 
-            if (rootNode.isArray()) {
-                ArrayNode arrayNode = (ArrayNode) rootNode;
+            Document document = new Document();
 
+            // Store data in its own document
+            JsonNode dataNode = rootNode.get("data");
+            if (dataNode != null && dataNode.isArray() && dataNode.size() > 0) {
+                ArrayNode arrayNode = (ArrayNode) dataNode;
+                List<Document> dataList = new ArrayList<>();
                 for (JsonNode jsonNode : arrayNode) {
-                    Document document = Document.parse(jsonNode.toString());
-                    collection.insertOne(document);
+                    Document dataDoc = Document.parse(jsonNode.toString());
+                    dataList.add(dataDoc);
                 }
-
-                return ResponseEntity.ok("Results stored in database");
-            } else {
-                Document document = Document.parse(result);
-                collection.insertOne(document);
-                return ResponseEntity.ok("Result stored in database");
+                document.put("data", dataList);
             }
+
+            // Store headers in its own document
+            Enumeration<String> headerNames = request.getHeaderNames();
+            Document headersDoc = new Document();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                headersDoc.put(headerName, request.getHeader(headerName));
+            }
+            document.put("headers", headersDoc);
+
+            // Store URL in its own document
+            String requestURL = request.getRequestURL().toString();
+            document.put("url", requestURL);
+
+            collection.insertOne(document);
+
+            return ResponseEntity.ok("Result stored in database");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error storing result in database: " + e.getMessage());
         }
     }
+*/
+   /*@PostMapping("/storeTestResults")
+   public ResponseEntity<String> storeResult(@RequestBody String result, @RequestHeader HttpHeaders headers, HttpServletRequest request) {
+       try {
+           ObjectMapper objectMapper = new ObjectMapper();
+           JsonNode rootNode = objectMapper.readTree(result);
+
+           if (rootNode.isArray()) {
+               ArrayNode arrayNode = (ArrayNode) rootNode;
+
+               for (JsonNode jsonNode : arrayNode) {
+                   Document document = new Document();
+                   document.append("data", jsonNode.toString());
+                   document.append("headers", headers.toString());
+                   document.append("url", request.getRequestURI());
+                   collection.insertOne(document);
+               }
+
+               return ResponseEntity.ok("Results stored in database");
+           } else {
+               Document document = new Document();
+               document.append("data", result);
+               document.append("headers", headers.toString());
+               document.append("url", request.getRequestURI());
+               collection.insertOne(document);
+               return ResponseEntity.ok("Result stored in database");
+           }
+       } catch (Exception e) {
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                   .body("Error storing result in database: " + e.getMessage());
+       }
+   }
+*/
+   @PostMapping("/storeTestResults")
+   public ResponseEntity<String> storeResult(@RequestBody String result, @RequestHeader HttpHeaders headers, HttpServletRequest request) {
+       try {
+           ObjectMapper objectMapper = new ObjectMapper();
+           JsonNode rootNode = objectMapper.readTree(result);
+
+           String url = rootNode.path("config").path("url").asText();
+           if (rootNode.isArray()) {
+               ArrayNode arrayNode = (ArrayNode) rootNode;
+
+               for (JsonNode jsonNode : arrayNode) {
+                   Document document = Document.parse(jsonNode.toString());
+                   document.append("headers", headers.toString());
+                   document.append("api",rootNode.path("config").path("url").asText());
+                   collection.insertOne(document);
+               }
+
+               return ResponseEntity.ok("Results stored in database");
+           } else {
+               Document document = Document.parse(result);
+               document.append("headers", headers.toString());
+               document.append("api", rootNode.path("config").path("url").asText());
+               collection.insertOne(document);
+               return ResponseEntity.ok("Result stored in database");
+           }
+       } catch (Exception e) {
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                   .body("Error storing result in database: " + e.getMessage());
+       }
+   }
+    private MongoCollection<Document> getMongoCollection() {
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase database = mongoClient.getDatabase("jwtRoleBasedAuth");
+        MongoCollection<Document> collection = database.getCollection("apiTestResults");
+        return collection;
+    }
+
+ /*   @GetMapping("/testResults")
+    public ResponseEntity<List<Document>> getTestResults() {
+        try {
+            MongoCollection<Document> collection = getMongoCollection(); // Replace with your own logic to get the collection
+            FindIterable<Document> results = collection.find();
+            List<Document> resultList = new ArrayList<>();
+
+            for (Document result : results) {
+                resultList.add(result);
+            }
+
+            return ResponseEntity.ok(resultList);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Collections.emptyList());
+        }
+    }
+
+*/
 
 
     @GetMapping("/testResults")
--- src/main/java/com/youtube/jwt/entity/ApiTest.java
+++ src/main/java/com/youtube/jwt/entity/ApiTest.java
@@ -1,4 +1,42 @@
 package com.youtube.jwt.entity;
 
+import java.util.Map;
+
 public class ApiTest {
+    private String url;
+    private Map<String, String> headers;
+    private String body;
+    private Object result;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
 }
